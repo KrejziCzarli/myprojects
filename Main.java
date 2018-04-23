1613f795ca7fe3fa5a24c540deb156b834411d38
KrejziCzarli <38221804+KrejziCzarli@users.noreply.github.com>
2018-04-23 15:51:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package pl.karolbil.udemy.firstprogram;
 //to jest tekst który nic nie zmieni
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Intellij!");
+        Systm.out.println("Hello, Intellij!");
 
     }
 }
